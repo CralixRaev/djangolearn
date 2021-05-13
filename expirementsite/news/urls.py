a979ce122ca021e195f39987110c716499eee221
cralixraev <cralixraev@gmail.com>
2021-05-13 21:07:59 +0300

    update

--- expirementsite/news/urls.py
+++ expirementsite/news/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import index, test
+from .views import index, get_category
 
 urlpatterns = [
-    path('', index),
-    path('test/', test),
+    path('', index, name='home'),
+    path('category/<int:category_id>/', get_category, name='category')
 ]
--- expirementsite/news/views.py
+++ expirementsite/news/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News, Category
+
 
 def index(request):
-    # print(request)
-    return HttpResponse('Hello world!')
+    news = News.objects.all()
+    context = {'news': news,
+               'title': 'Список новостей'}
+    return render(request, 'news/index.html', context)
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    context = {'news': news,
+               'category': category}
+    return render(request, 'news/category.html', context)
